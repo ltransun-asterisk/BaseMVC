e12f88b5e52a4f85cc81fcec99f32741cef0e8b1
ltranframgia <ltranframgia@gmail.com>
2017-06-22 00:24:37 +0700

    add location manager add flow app (Login , mainapp, ...)

--- BaseMVC/Sources/Helpers/StructDefine.swift
+++ BaseMVC/Sources/Helpers/StructDefine.swift
@@ -1,11 +1,3 @@
-struct DateFormat {
-    static let yyyyssDash = "yyyy-MM-dd'T'HH:mm:ss"
-    static let ddmmSlash = "dd/MM/yyyy HH:mm"
-    static let ddMMyyyy = "dd/MM/yyyy"
-    static let yyyyMMddDash = "yyyy-MM-dd"
-    static let MMyyyy = "MM/yyyy"
-}
-
 public struct GroupItem<Group, Item> {
     public var group: Group
     public var items: [Item]
